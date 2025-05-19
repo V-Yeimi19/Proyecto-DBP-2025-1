b2961466901b539c1caa34b565360c2d87d4e90b
JuLiOp08 <julio.portugal@utec.edu.pe>
2025-05-18 19:25:07 -0500

    Añadiendo los Enums necesarios para cada entidad

---
+++ WatchApp/WatchApp/src/main/java/com/example/watchapp/domain/NotificationStatus.java
@@ -0,0 +1,5 @@
+package com.example.watchapp.domain;
+
+public enum NotificationStatus {
+    SENT, READ
+}
---
+++ WatchApp/WatchApp/src/main/java/com/example/watchapp/domain/Status.java
@@ -0,0 +1,5 @@
+package com.example.watchapp.domain;
+
+public enum Status {
+    REPORTED, IN_PROGRESS, RESOLVED
+}
